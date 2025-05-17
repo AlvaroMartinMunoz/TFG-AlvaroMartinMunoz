5f0a00c04b665671844ba2ca60bb05512cefe446
Alvaro Martin Muñoz <alvmarmun1@alum.us.es>
2025-05-17 16:22:08 +0200

    fix: correccion en el css de los detalles de una propiedad

--- marketplace/frontend/src/components/PropertyDetails.js
+++ marketplace/frontend/src/components/PropertyDetails.js
@@ -1510,7 +1510,8 @@ const PropertyDetails = () => {
                                 flexDirection: 'column',
                                 justifyContent: 'space-between', // Distribuye espacio
                                 position: 'relative',
-                                boxShadow: '0 6px 20px rgba(0,0,0,0.1)'
+                                boxShadow: '0 6px 20px rgba(0,0,0,0.1)',
+                                width: '100%',
                             }}
                         >
                             {/* Contenido Principal Info */}
